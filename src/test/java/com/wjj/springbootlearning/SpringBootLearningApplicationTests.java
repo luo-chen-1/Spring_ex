9293674b1b1b107b91e6220b967f8f31c382c5f1
luo-chen-1 <2710605493@qq.com>
2023-03-14 17:11:53 +0800

    Spring-boot-config-learning

---
+++ src/test/java/com/wjj/springbootlearning/SpringBootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjj.springbootlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
